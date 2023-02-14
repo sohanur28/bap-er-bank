de800fbe2b0e506f48fbad555276fea784652059
Sohanur Rahman <gsrahman37@gmail.com>
2023-02-15 00:34:08 +0600

    bank

--- js/login.js
+++ js/login.js
@@ -15,10 +15,10 @@ document.getElementById('btn-submit').addEventListener('click', function(){
      // DANGER: DO NOT VERIFY email password on the client side
      // step-4: varify email and password and check whether valid user or not
      if(email === 'bank@tk.com' && password === 'secret'){
-         console.log('valid user');
+        window.location.href = 'bank.html';
      }
      else{
-         console.log('invalid user');
+         alert('invalid user');
      }
 
 })
